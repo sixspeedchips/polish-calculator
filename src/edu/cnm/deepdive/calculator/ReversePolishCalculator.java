0941126a15a9340b9535dd1868d96dd5c55aab60
Sami <samimheard@gmail.com>
2019-09-16 17:26:12 -0600

    Merge branch 'master' of C:\Users\samim\Desktop\bootcamp\projects\polish-calculator with conflicts.

--- src/edu/cnm/deepdive/calculator/ReversePolishCalculator.java
+++ src/edu/cnm/deepdive/calculator/ReversePolishCalculator.java
@@ -2,8 +2,4 @@ package edu.cnm.deepdive.calculator;
 
 public class ReversePolishCalculator implements Calculator {
 
-  @Override
-  public Integer calculate(String postfixString) {
-    return null;
-  }
 }
