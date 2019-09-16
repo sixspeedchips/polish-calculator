1ba676a37e549878fe152eb014cf1734b2840034
Sami <samimheard@gmail.com>
2019-09-16 17:31:32 -0600

    added handling of operators and stack calculate

--- src/edu/cnm/deepdive/calculator/ReversePolishCalculator.java
+++ src/edu/cnm/deepdive/calculator/ReversePolishCalculator.java
@@ -1,6 +1,7 @@
 package edu.cnm.deepdive.calculator;
 
 import java.util.Arrays;
+import java.util.LinkedList;
 
 public class ReversePolishCalculator implements Calculator {
 
@@ -15,5 +16,32 @@ public class ReversePolishCalculator implements Calculator {
   private String[] parse(String inputString) {
     return inputString.strip().split(" ");
   }
+
+  String x = "  1 1 23 5";
+
+  String[] arrayofs x.strip().split(" ");
+  LinkedList<String> stack = new LinkedList<>(String);
+    for (ele : arrayofs){
+    if (ele == "+"){
+      int op1 = Integer.parseInt(stack.pop());
+      int op2 = Integer.parseInt(stack.pop());
+      stack.push(String.valueOf(op1+op2));
+    } else if (ele == "-"){
+      int op1 = Integer.parseInt(stack.pop());
+      int op2 = Integer.parseInt(stack.pop());
+      stack.push(String.valueOf(op1-op2));
+    } else if (ele == "*"){
+      int op1 = Integer.parseInt(stack.pop());
+      int op2 = Integer.parseInt(stack.pop());
+      stack.push(String.valueOf(op1*op2));
+    } else if (ele == "/"){
+      int op1 = Integer.parseInt(stack.pop());
+      int op2 = Integer.parseInt(stack.pop());
+      stack.push(String.valueOf(op1/op2));
+    } else{
+      stack.push(ele);
+    }
+  }
+    System.out.println(stack);
 
 }
