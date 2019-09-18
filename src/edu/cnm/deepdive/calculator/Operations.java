a1bcf4524f48a12100f7d8e94c9ad92d4ffa4081
finn <isaaclindland@protonmail.com>
2019-09-18 10:49:00 -0600

    Made all operator functions lambdas

--- src/edu/cnm/deepdive/calculator/Operations.java
+++ src/edu/cnm/deepdive/calculator/Operations.java
@@ -1,7 +1,6 @@
 package edu.cnm.deepdive.calculator;
 
 import java.util.HashMap;
-import java.util.LinkedList;
 import java.util.Map;
 
 class Operations {
@@ -9,71 +8,35 @@ class Operations {
 
   final static Map<String,Operator> operators = new HashMap<>();
   static {
-    operators.put("*", Multiply.instance());
-    operators.put("+", Add.instance());
-    operators.put("-", Subtract.instance());
-    operators.put("/", Divide.instance());
-  }
-
-
-  static class Multiply implements Operator {
-
-    private Multiply(){
-    }
-    @Override
-    public void operate(LinkedList<Double> stack) {
+    operators.put("*",stack -> {
       Double operandOne = stack.pop();
       Double operandTwo = stack.pop();
       stack.push(operandOne * operandTwo);
-    }
-
-    static Multiply instance(){
-      return new Multiply();
-    }
-
-  }
-
-  static class Divide implements Operator{
-
-    private Divide() {
-    }
-
-    @Override
-    public void operate(LinkedList<Double> stack) {
+    });
+    operators.put("/",stack ->{
       Double operandOne = stack.pop();
       Double operandTwo = stack.pop();
-
       stack.push(operandTwo / operandOne);
-    }
-    static Divide instance(){
-      return new Divide();
-    }
-  }
-
-  static class Add implements Operator{
-
-    @Override
-    public void operate(LinkedList<Double> stack) {
-      Double operandOne = stack.pop();
-      Double operandTwo = stack.pop();
-      stack.push(operandOne + operandTwo);
-    }
-    static Add instance(){
-      return new Add();
-    }
-  }
-
-  static class Subtract implements Operator{
-
-    @Override
-    public void operate(LinkedList<Double> stack) {
+    });
+    operators.put("-",stack -> {
       Double operandOne = stack.pop();
       Double operandTwo = stack.pop();
       stack.push(operandTwo - operandOne);
-    }
-
-    static Subtract instance(){
-      return new Subtract();
-    }
+    });
+    operators.put("+", stack -> {
+      Double operandOne = stack.pop();
+      Double operandTwo = stack.pop();
+      stack.push(operandOne + operandTwo);
+    });
+    operators.put("sqrt",stack -> {
+      Double operandOne = stack.pop();
+      stack.push(Math.sqrt(operandOne));
+    });
+    operators.put("^",stack -> {
+      Double operandOne = stack.pop();
+      Double operandTwo = stack.pop();
+      stack.push(Math.pow(operandTwo, operandOne));
+    });
   }
+
 }
--- src/edu/cnm/deepdive/calculator/Operator.java
+++ src/edu/cnm/deepdive/calculator/Operator.java
@@ -1,6 +1,7 @@
 package edu.cnm.deepdive.calculator;
 
 import java.util.LinkedList;
+import java.util.Stack;
 
 interface Operator {
 
